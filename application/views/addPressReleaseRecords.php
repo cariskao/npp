b332767d23e0ec06781ee79199842fab570836a9
caris <hgfhhshgg@gmail.com>
2019-12-23 06:52:41 +0800

    ok

--- application/views/addPressReleaseRecords.php
+++ application/views/addPressReleaseRecords.php
@@ -38,14 +38,14 @@
 										<?php echo form_error('file'); ?>
 									</div>
 								</div>
-								<div class="col-md-12">
+								<div class="col-md-6">
 									<div class="form-group">
 										<label for="m_title">大標</label>
 										<input type="text" class="form-control" id="m_title" name="m_title" value="">
 										<?php echo form_error('m_title'); ?>
 									</div>
 								</div>
-								<div class="col-md-12">
+								<div class="col-md-6">
 									<div class="form-group">
 										<label for="s_title">次標</label>
 										<input type="text" class="form-control" id="s_title" name="s_title" value="">
